8fcc813b4c0193b32aa846307fbf6c7046f8bca0
ganganyyy <ganganyyy0612@gmail.com>
2019-07-20 15:25:19 +0800

    干干   修改跳转路径

--- emp/WebRoot/js/jump.js
+++ emp/WebRoot/js/jump.js
@@ -2,6 +2,17 @@
  * @author:刘诗滢 吴佳珊
  */
 
+/**
+ * 跳转到个人中心
+ * @param vaab101
+ * @returns
+ */
+function personal(vaab101)
+{
+	var vform = document.getElementById("myform");
+ 	vform.action="/emp/queryMessage.html?aab101="+vaab101;
+ 	vform.submit();
+}
 
 /**
  * 跳转到食谱具体界面
@@ -12,7 +23,7 @@ function toMenuDetail(aac101)
 {
 
 	var form=document.getElementById("myform");
-	alert("/emp/details.html?aac101="+aac101);
+	//alert("/emp/details.html?aac101="+aac101);
 	form.action="/emp/details.html?aac101="+aac101;
 	form.submit();
 	
@@ -26,7 +37,68 @@ function toMenuDetail(aac101)
 function kitchen(vaab101)
 {
 	var vform = document.getElementById("myform");
-	alert("/emp/queryHomeRecipe.html?aab101="+vaab101);
+	//alert("/emp/queryHomeRecipe.html?aab101="+vaab101);
  	vform.action="/emp/queryHomeRecipe.html?aab101="+vaab101;
  	vform.submit();
 }
+
+/**
+ * 跳转
+ * @returns
+ */
+function toAllWork()
+{
+	var vform = document.getElementById("myform");
+	alert("/emp/queryVoteDetail.html?aae101="+vaae101);
+ 	vform.action="/emp/queryVoteDetail.html?aae101="+vaae101;
+ 	vform.submit();
+
+}
+
+/**
+ * 跳转到食谱一览
+ * @param type
+ * @returns
+ */
+function selectType(type)
+{
+	changeClass("option2");
+	document.getElementById
+	var form=document.getElementById("myform");
+	form.action="/emp/allMenu.html?aac107="+type;
+	form.submit();
+	
+}
+
+/**
+ * 改变toggle选中状态
+ * @param id
+ * @returns
+ */
+function changeClass(id)
+{
+	alert(id);
+	for(var i=1;i<=5;i++)
+	{
+		var option="option"+i;
+		alert(option);
+		document.getElementById(option).class="";
+	}
+	document.getElementById(id).class="active";
+	
+	alert(document.getElementById(id).class);
+}
+
+/**
+ * 跳转到所有活动页面
+ * @param vaab101
+ * @returns
+ */
+function ActiDetail(vaae101)
+{
+	var vform = document.getElementById("myform");
+	//alert("/emp/queryVoteDetail.html?aae101="+vaae101);
+ 	vform.action="/emp/queryVoteDetail.html?aae101="+vaae101;
+ 	vform.submit();
+}
+
