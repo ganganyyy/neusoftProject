595d22bb6e5e0fa303d9344800a2c247cdbd522e
sandysandrawu <sandysandrawu@163.com>
2019-07-20 15:27:38 +0800

    33

--- emp/src/com/neusoft/services/impl/Ac02ServicesImpl.java
+++ emp/src/com/neusoft/services/impl/Ac02ServicesImpl.java
@@ -8,14 +8,14 @@ import java.util.Random;
 import com.neusoft.services.JdbcServicesSupport;
 import com.neusoft.system.tools.Tools;
 /**
- * ��Ʒ��ز���
- * @author ������
+ * 锟斤拷品锟斤拷夭锟斤拷锟�
+ * @author 锟斤拷锟斤拷锟斤拷
  */
 public class Ac02ServicesImpl extends JdbcServicesSupport
 {
 
 	/**
-     * ɺɺͷ��
+     * 珊珊头锟斤拷
      * @return
      * @throws Exception
      */
@@ -25,46 +25,24 @@ public class Ac02ServicesImpl extends JdbcServicesSupport
 		return this.queryForMap(sql,this.get("aab101"));
 	}
 
-	   //��ȡ�ղؼ�ͼƬ
-	public String getImg()
-	{
-		Random ran = new Random();
-	    int i = ran.nextInt(5);
-	    switch(i)
-	    {
-	         case 1:
-	            return "img/collection/01.jpeg";
-			 case 2:
-				return "img/collection/02.jpg";
-			 case 3:
-				return "img/collection/03.jpg";
-			 case 4:
-			    return "img/collection/04.jpg";
-			 case 5:
-			    return "img/collection/05.jpeg";
-			 default:
-				return "img/collection/06.jpg";
-	    }
-	}
-
-	//��ѯ������Ʒ
+	//锟斤拷询锟斤拷锟斤拷锟斤拷品
     public List<Map<String,String>> query()throws Exception
     {
-    	//1.��дSQL���
+    	//1.锟斤拷写SQL锟斤拷锟�
     	StringBuilder sql=new StringBuilder()
     			.append("select aac201,aab101,aac202,aac203,aac204,")
     			.append("       aac207,aac205,aac206 ")
     			.append("  from ac02 ")
     			;
-    	//ִ�в�ѯ
+    	//执锟叫诧拷询
     	return this.queryForList(sql.toString());
     }
 
 
-    //��ѯĳһ��Ʒ����
+    //锟斤拷询某一锟斤拷品锟斤拷锟斤拷
     public Map<String,String> findById()throws Exception
     {
-    	//1.��дSQL���
+    	//1.锟斤拷写SQL锟斤拷锟�
     	StringBuilder sql=new StringBuilder()
     			.append("select a.aac201,a.aab101,a.aac202,a.aac203,a.aac204,")
     			.append("       a.aac207,c.aac102,b.aab101,b.aab102,b.aab106")
@@ -73,7 +51,7 @@ public class Ac02ServicesImpl extends JdbcServicesSupport
     			.append("   and a.aab101=b.aab101")
     			.append("   and a.aac201=?")
     			;
-    	//ִ�в�ѯ
+    	//执锟叫诧拷询
     	Map<String,String> abc = this.queryForMap(sql.toString(), this.get("aac201"));
     	String likeNumber=likeNumber();
     	abc.put("aad101", likeNumber);
@@ -81,7 +59,7 @@ public class Ac02ServicesImpl extends JdbcServicesSupport
     	abc.put("aad201", collectionNumber);
     	return abc;
     }
-    //��ѯ�ղؼ�
+    //锟斤拷询锟秸藏硷拷
 	public List<Map<String,String>> queryCollections()throws Exception
 	{
 		 StringBuilder sql=new StringBuilder()
@@ -94,19 +72,19 @@ public class Ac02ServicesImpl extends JdbcServicesSupport
 
 
     /*
-     * �ϴ���Ʒinsert into ac02(aab101,aac202,aac203,aac204,aac207,aac205,aac206)
-     * values('1',NOW(),'������������������','img/san.jpg','1', '0','0')
+     * 锟较达拷锟斤拷品insert into ac02(aab101,aac202,aac203,aac204,aac207,aac205,aac206)
+     * values('1',NOW(),'锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷','img/san.jpg','1', '0','0')
      */
     public boolean addPro(Map<String,Object> dto)throws Exception
     {
-    	//1.��дSQL���
+    	//1.锟斤拷写SQL锟斤拷锟�
     	StringBuilder sql=new StringBuilder()
     			.append("insert into ac02(aab101,aac202,aac203,aac204,aac207,")
     			.append("                 aac205,aac206)")
     			.append("          values(?,NOW(),?,?,?,")
     			.append("                 '0','0')")
     			;
-    	//2.��д��������
+    	//2.锟斤拷写锟斤拷锟斤拷锟斤拷锟斤拷
     	Object args[]={
     			this.get("aab101Self"),
     			dto.get("aac203"),
@@ -116,7 +94,7 @@ public class Ac02ServicesImpl extends JdbcServicesSupport
         return this.executeUpdate(sql.toString(), args)>0;
     }
 
-    //�ж��Ƿ��ղ�
+    //锟叫讹拷锟角凤拷锟秸诧拷
     private String collectionNumber()throws Exception
     {
     	StringBuilder sql=new StringBuilder()
@@ -129,21 +107,21 @@ public class Ac02ServicesImpl extends JdbcServicesSupport
     	Object args[]={this.get("aac201"),this.get("aab101Self")};
     	return check(sql.toString(),args);
     }
-    //�ж��Ƿ����
+    //锟叫讹拷锟角凤拷锟斤拷锟�
     private String likeNumber()throws Exception
     {
     	String sql="select aad101 from ad01 where aad103='02' and aad104=? and aab101=? ";
     	Object args[]={this.get("aac201"),this.get("aab101Self")};
     	return check(sql,args);
     }
-    //�����Ʒ���û���ˮ��
+    //锟斤拷锟斤拷锟狡凤拷锟斤拷没锟斤拷锟剿拷锟�
     private String authorNumber()throws Exception
     {
     	String sql="select aab101 from ac02 where aac201=? ";
     	Object args[]={this.get("aac201")};
     	return check(sql,args);
     }
-    //��õ�ǰ�û����û���
+    //锟斤拷玫锟角帮拷没锟斤拷锟斤拷没锟斤拷锟�
     private String userName()throws Exception
     {
     	String sql="select aab102 from ab01 where aab101=? ";
@@ -152,10 +130,10 @@ public class Ac02ServicesImpl extends JdbcServicesSupport
     }
 
     /**
-     * ִ�в�ѯ��ֵ���
+     * 执锟叫诧拷询锟斤拷值锟斤拷锟�
      * @param sql
      * @param args
-     * @return ��ѯ�õ���Ψһ��ֵ
+     * @return 锟斤拷询锟矫碉拷锟斤拷唯一锟斤拷值
      * @throws Exception
      */
     private String check(String sql,Object args[])throws Exception
@@ -175,9 +153,9 @@ public class Ac02ServicesImpl extends JdbcServicesSupport
 
     private Object[] xiaoxi(String msg)throws Exception
     {
-    	//��ǰ��Ʒ���û���
+    	//锟斤拷前锟斤拷品锟斤拷锟矫伙拷锟斤拷
     	String authorNumber = authorNumber();
-    	//��ǰ�û�������
+    	//锟斤拷前锟矫伙拷锟斤拷锟斤拷锟斤拷
     	String userName=userName();
     	String text=userName+msg;
     	Object args[]={authorNumber,text};
@@ -185,10 +163,10 @@ public class Ac02ServicesImpl extends JdbcServicesSupport
     }
 
     /*
-     * ������Ʒ
-     * 1������ޱ������ӵ�������
-     * 2��������Ʒ����ĵ�����
-     * 3������Ϣ�б���������
+     * 锟斤拷锟斤拷锟斤拷品
+     * 1锟斤拷锟斤拷锟斤拷薇锟斤拷锟斤拷锟斤拷拥锟斤拷锟斤拷锟斤拷锟�
+     * 2锟斤拷锟斤拷锟斤拷锟斤拷品锟斤拷锟斤拷牡锟斤拷锟斤拷锟�
+     * 3锟斤拷锟斤拷锟斤拷息锟叫憋拷锟斤拷锟斤拷锟斤拷锟斤拷
      */
     private boolean giveLike()throws Exception
     {
@@ -214,13 +192,13 @@ public class Ac02ServicesImpl extends JdbcServicesSupport
     			.append("insert into ab03(aab101,aab302,aab303,aab304)")
     			.append("          values(?,?,'00',NOW())")
     			;
-    	Object args3[]=xiaoxi("�������Ʒ������");
+    	Object args3[]=xiaoxi("锟斤拷锟斤拷锟斤拷锟狡凤拷锟斤拷锟斤拷锟�");
     	this.apppendSql(sql3.toString(), args3);
 
     	return this.executeTransaction();
     }
 
-    //ȡ����Ʒ����
+    //取锟斤拷锟斤拷品锟斤拷锟斤拷
     private boolean cancleProLike()throws Exception
     {
     	String sql1="delete from ad01 where aad103='02' and aab101=? and aad104=? ";
@@ -239,15 +217,15 @@ public class Ac02ServicesImpl extends JdbcServicesSupport
     	return this.executeTransaction();
     }
 
-    //������Ʒ
+    //锟斤拷锟斤拷锟斤拷品
     private boolean addComment()throws Exception
     {
-    	//1.��дSQL���
+    	//1.锟斤拷写SQL锟斤拷锟�
     	StringBuilder sql1=new StringBuilder()
     			.append("insert into ad04(aad402,aad403,aad404,aad405)")
     			.append("          values('02',?,?,?)")
     			;
-    	//2.��д��������
+    	//2.锟斤拷写锟斤拷锟斤拷锟斤拷锟斤拷
     	Object args1[]={
     			this.get("aac201"),
     			this.get("aad404"),
@@ -259,13 +237,13 @@ public class Ac02ServicesImpl extends JdbcServicesSupport
     			.append("insert into ab03(aab101,aab302,aab303,aab304)")
     			.append("          values(?,?,'00',NOW())")
     			;
-    	Object args2[]=xiaoxi("�����������Ʒ");
+    	Object args2[]=xiaoxi("锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟狡�");
     	this.apppendSql(sql2.toString(), args2);
 
     	return this.executeTransaction();
     }
 
-    //չʾ����
+    //展示锟斤拷锟斤拷
     public List<Map<String,String>> queryComment()throws Exception
     {
     	StringBuilder sql=new StringBuilder()
@@ -281,7 +259,7 @@ public class Ac02ServicesImpl extends JdbcServicesSupport
     	return this.queryForList(sql.toString(),args);
     }
 
-    //չʾ����
+    //展示锟斤拷锟斤拷
     public List<Map<String,String>> queryLikes()throws Exception
     {
     	StringBuilder sql=new StringBuilder()
@@ -297,7 +275,7 @@ public class Ac02ServicesImpl extends JdbcServicesSupport
     	return this.queryForList(sql.toString(),args);
     }
 
-    //�ղ���Ʒ
+    //锟秸诧拷锟斤拷品
     private boolean collection()throws Exception
     {
     	StringBuilder sql1=new StringBuilder()
@@ -322,13 +300,13 @@ public class Ac02ServicesImpl extends JdbcServicesSupport
     			.append("insert into ab03(aab101,aab302,aab303,aab304)")
     			.append("          values(?,?,'00',NOW())")
     			;
-    	Object args3[]=xiaoxi("�ղ��������Ʒ");
+    	Object args3[]=xiaoxi("锟秸诧拷锟斤拷锟斤拷锟斤拷锟狡�");
     	this.apppendSql(sql3.toString(), args3);
 
     	return this.executeTransaction();
     }
 
-	//��ȡ�ղؼ�ͼƬ
+	//锟斤拷取锟秸藏硷拷图片
 	public String getImg()
 	{
 		Random ran = new Random();
@@ -350,7 +328,7 @@ public class Ac02ServicesImpl extends JdbcServicesSupport
 	    }
 	}
 
-    //�����������ղؼ�
+    //锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟秸藏硷拷
 	public boolean createColl()throws Exception
 	{
 		 StringBuilder sql=new StringBuilder()
@@ -380,7 +358,7 @@ public class Ac02ServicesImpl extends JdbcServicesSupport
 	     return this.executeTransaction();
 	}
 
-    //ȡ���ղ�
+    //取锟斤拷锟秸诧拷
     private boolean cancleCollection()throws Exception
     {
     	String sql1="delete from ad02 where aad201=? ";
@@ -399,14 +377,14 @@ public class Ac02ServicesImpl extends JdbcServicesSupport
     	return this.executeTransaction();
     }
 
-    //ɾ����Ʒ
+    //删锟斤拷锟斤拷品
     private boolean deleteById()throws Exception
     {
     	String sql="delete from ac02 where aac201=?";
     	return this.executeUpdate(sql, this.get("aac201"))>0;
     }
 
-    //�޸���Ʒ
+    //锟睫革拷锟斤拷品
     public boolean modifyPro(Map<String,Object> dto)throws Exception
     {
     	StringBuilder sql=new StringBuilder()
@@ -422,7 +400,7 @@ public class Ac02ServicesImpl extends JdbcServicesSupport
     	return this.executeUpdate(sql.toString(), args)>0;
     }
 
-    //��ѯ�ҵ��޵Ĳ���
+    //锟斤拷询锟揭碉拷锟睫的诧拷锟斤拷
     private List<Map<String,String>> myReciLikes()throws Exception
     {
 
@@ -437,7 +415,7 @@ public class Ac02ServicesImpl extends JdbcServicesSupport
 
     	return this.queryForList(sql.toString(),this.get("aab101"));
     }
-    //��ѯ�ҵ��޵���Ʒ
+    //锟斤拷询锟揭碉拷锟睫碉拷锟斤拷品
     private List<Map<String,String>> myProLikes()throws Exception
     {
 
@@ -451,7 +429,7 @@ public class Ac02ServicesImpl extends JdbcServicesSupport
 
     	return this.queryForList(sql.toString(),this.get("aab101"));
     }
-    //��ѯ�ҵ��޵�ר��
+    //锟斤拷询锟揭碉拷锟睫碉拷专锟斤拷
     private List<Map<String,String>> myCompLikes()throws Exception
     {
     	StringBuilder sql=new StringBuilder()
@@ -465,7 +443,7 @@ public class Ac02ServicesImpl extends JdbcServicesSupport
     	return this.queryForList(sql.toString(),this.get("aab101"));
     }
 
-    //ɾ������
+    //删锟斤拷锟斤拷锟斤拷
     private boolean deleteLikes()throws Exception
     {
     	String tag=(String)this.get("aad103");
